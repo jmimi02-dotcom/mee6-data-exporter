7869d24e825191e2a1f85223832db53fc41d96a9
luisjones <11049493+luisjones@users.noreply.github.com>
2025-07-28 00:25:14 +0100

    fix: remove unused import

--- main.go
+++ main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
+	_ "github.com/mattn/go-sqlite3"
 	"mee6xport/db"
 	"mee6xport/mee6"
-        "mee6xport/ui"
-
-	_ "github.com/mattn/go-sqlite3"
 )
 
 var guildID = /* Change this value as per README.md */ 1234
 
 func main() {
-        //ui.LaunchProgram()
 	_, tx := db.PrepareDB()
 	pages, _ := mee6.CrawlGuild(guildID)
 	for _, page := range pages {
